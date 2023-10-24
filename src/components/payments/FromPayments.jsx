65a35c5f33fcd4be60de0f929c2057fb2c5a72a9
Jesus-Garcia-Montalvo <jesus.antonio.garcia9871@gmail.com>
2023-10-24 00:45:14 -0500

    agrege FromPayments

---
+++ src/components/payments/FromPayments.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import InputWithValidation from "../input/InputWithValidation";
+import { InfoOutlineIcon } from "@chakra-ui/icons";
+import Title from "../title/Title";
+import "./FromPayments.css";
+import SelectWithValidation from "../select/SelectWithValidation";
+import { Button, Checkbox } from "@chakra-ui/react";
+
+const FromPayments = () => {
+  return (
+    <form className="form" style={{ display: "flex", flexDirection: "column" }}>
+      <h1 className="title">MyBoking</h1>
+      <Title
+        text="Payment method"
+        iconColor="blue.500"
+        textColor="blue.500"
+        textSize="30px" // Tamaño del texto
+      />
+      <div className="cont">
+        <InputWithValidation
+          label="Nombre"
+          type="text"
+          name="nombre"
+          width="300px"
+          placeholder="Enter name on card"
+        />
+        <InputWithValidation
+          label="Card number"
+          type="text"
+          name="Card number"
+          width="300px"
+          placeholder="Enter card number"
+        />
+      </div>
+      <div className="cont">
+        <SelectWithValidation
+          label="Expiration date"
+          name="Title"
+          options={["Selects", "United Arab Emirates", "Nigeria"]}
+          width="300px"
+          placeholder="input Texto"
+        />
+        <InputWithValidation
+          label="CVV"
+          type="text"
+          name="CVV"
+          width="300px"
+          placeholder="Enter CVV"
+        />
+      </div>
+      <Checkbox defaultChecked>Use same address as billing info</Checkbox>
+      <InputWithValidation
+        label="Address"
+        type="text"
+        name="Address"
+        width="100%"
+        placeholder="Add address"
+      />
+      <div className="cont">
+        <InputWithValidation
+          label="Zip/Postal code"
+          type="text"
+          name="Address"
+          width="300px"
+          placeholder="Zip/Postal code"
+        />
+        <SelectWithValidation
+          label="Nationality"
+          name="Title"
+          options={["Selects", "United Arab Emirates", "Nigeria"]}
+          width="300px"
+          placeholder="Select"
+        />
+      </div>
+      <Button colorScheme="green">Mostrar Datos</Button>
+    </form>
+  );
+};
+
+export default FromPayments;
